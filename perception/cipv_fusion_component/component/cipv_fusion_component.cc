a09e2b464257c7ca9d709bf1b036462faaaa885d
tang <122545505+1121085292@users.noreply.github.com>
2023-07-12 17:08:50 +0800

    Add files via upload

---
+++ perception/cipv_fusion_component/component/cipv_fusion_component.cc
@@ -0,0 +1,9 @@
+#include "cipv_fusion_component.h"
+
+bool CipvComponent::Init() {
+    return true;
+}
+
+bool CipvComponent::Proc(const std::shared_ptr<SensorFrameMessage>& message) {
+
+}
---
+++ perception/cipv_fusion_component/component/cipv_fusion_component.h
@@ -0,0 +1,14 @@
+// 1.包含被依赖的其他头文件
+#include "cyber/component/component.h"
+#include "proto/cipv_fusion.pb.h"
+
+using apollo::cyber::component;
+// 2.自定义类继承 Component
+class CipvComponent : pubilc Component<SensorFrameMessage> {
+  pubilc:
+    bool Init() override;
+    bool Proc(const std::shared_ptr<SensorFrameMessage>& message) override;
+};
+
+// 注册组件
+CYBER_REGISTER_COMPONENT(CipvComponent);
