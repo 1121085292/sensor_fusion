8190b37c2b608e443b14d83fbb2a7351757be475
tang <122545505+1121085292@users.noreply.github.com>
2023-07-12 22:49:07 +0800

    Update cipv_fusion_component.cc

--- perception/cipv_fusion_component/component/cipv_fusion_component.cc
+++ perception/cipv_fusion_component/component/cipv_fusion_component.cc
@@ -1,9 +1,13 @@
-#include "cipv_fusion_component.h"
+#include "cipv_fusion_component/component/cipv_fusion_component.h"
 
 bool CipvComponent::Init() {
+    AINFO << " ---------INIT---------";
     return true;
 }
 
-bool CipvComponent::Proc(const std::shared_ptr<SensorFrameMessage>& message) {
-
+bool CipvComponent::Proc(const std::shared_ptr<LeadDataV3>& camera, 
+                        const std::shared_ptr<RadarState>& radar, 
+                        const std::shared_ptr<CarState>& car) {
+    AINFO << "camera";
+    return true;
 }
