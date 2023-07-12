34e5f76dc31ac4c37c9cb252904aebff275030e5
tang <122545505+1121085292@users.noreply.github.com>
2023-07-12 22:48:43 +0800

    Update cipv_fusion_component.h

--- perception/cipv_fusion_component/component/cipv_fusion_component.h
+++ perception/cipv_fusion_component/component/cipv_fusion_component.h
@@ -1,13 +1,22 @@
+#pragma once
 // 1.包含被依赖的其他头文件
 #include "cyber/component/component.h"
-#include "proto/cipv_fusion.pb.h"
 
-using apollo::cyber::component;
+#include "cipv_fusion_component/proto/cipv_fusion.pb.h"
+#include "cipv_fusion_component/proto/car_state.pb.h"
+
+using apollo::cyber::Component;
+using cipv_fusion_component::proto::LeadDataV3;
+using cipv_fusion_component::proto::RadarState;
+using cipv_fusion_component::proto::CarState;
+
 // 2.自定义类继承 Component
-class CipvComponent : pubilc Component<SensorFrameMessage> {
-  pubilc:
+class CipvComponent : public Component<LeadDataV3, RadarState, CarState> {
+  public:
     bool Init() override;
-    bool Proc(const std::shared_ptr<SensorFrameMessage>& message) override;
+    bool Proc(const std::shared_ptr<LeadDataV3>& camera,
+        const std::shared_ptr<RadarState>& radar,
+        const std::shared_ptr<CarState>& car) override;
 };
 
 // 注册组件
